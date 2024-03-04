c506694111a11b27acd2ce89923ee4e1ce773a9f
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-03-04 07:06:04 +0100

    feat: created index.js server entry file

---
+++ index.js
@@ -0,0 +1,44 @@
+//accessing & configuring environmental variables
+const dotenv = require('dotenv');
+dotenv.config();
+//Accepting from unauthorized
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+//variables
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8001;
+const cors = require('cors');
+const useragent = require('express-useragent');
+const swaggerUi = require("swagger-ui-express");
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+//using middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(useragent.express());
+app.use(express.static(__dirname + '/public'));
+
+//importing all required routes
+const authRoutes = require('./src/routes/auth');
+const errorHandler = require('./src/middleware/errorHandler');
+
+// Parse YAML Swagger documentation to JSON
+const swaggerFile = fs.readFileSync('./src/documentation/swagger.yaml', 'utf8');
+const swaggerDocument = yaml.load(swaggerFile);
+
+//using imported routes
+app.use(process.env.ROUTE_PREFIX, authRoutes);
+
+// Serve Swagger documentation at /api/docs
+app.use(process.env.API_DOCS_ROUTE_PREFIX, swaggerUi.serve);
+app.get(process.env.API_DOCS_ROUTE_PREFIX, swaggerUi.setup(swaggerDocument));
+
+// Use the errorHandler middleware
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`App successfully running on port ${port}`);
+})

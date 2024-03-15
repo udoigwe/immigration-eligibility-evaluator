08d2c393ec66236cee131a3a93e42b4f87540fa9
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-03-15 19:45:18 +0100

    chore: created nodemailer mailer functions

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,6 +2,7 @@ const pool = require("./dbConfig");
 const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
 const CustomError = require("./CustomError");
+const nodemailer = require('nodemailer');
 
 exports.validateEmail = (email) => {
     
@@ -118,6 +119,37 @@ exports.getCustomColumns = async (customQuery) => {
         const customColumns = result.fields.map(field => field.name);
 
         return customColumns;
+    }
+    catch(e)
+    {
+        throw e;
+    }
+}
+
+exports.sendMail = async (to, subject, html) => {
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 2525,
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: `"Immigration Evaluator" <${process.env.GMAIL_USER}>`,
+        to,
+        subject,
+        html
+    };
+
+    try
+    {
+        /* const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent:', info.response);
+        return true; // Email sent successfully */
+        await transporter.sendMail(mailOptions);
+        return true;
     }
     catch(e)
     {

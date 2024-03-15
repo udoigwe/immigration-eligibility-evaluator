142874ef3fac2bea4755e9b151f91b44fa91323f
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-03-15 19:50:33 +0100

    feat: created application routes validators

--- src/middleware/validators.js
+++ src/middleware/validators.js
@@ -1,8 +1,12 @@
 const { validate } = require('../utils/functions');
 const authValidations = require('../validations/auth.validation');
+const applicationValidations = require('../validations/application.validation');
 
 module.exports = {
     /* Auth route validators */
     signUp: validate(authValidations.signUp),
     signin: validate(authValidations.signin),
+
+    /* Applications route validators */
+    apply: validate(applicationValidations.apply)
 }

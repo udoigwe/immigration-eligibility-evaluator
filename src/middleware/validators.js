1da68aa86b68f5bf927a0bf1685850019441dbbd
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-03-22 06:29:56 +0100

    sprint 1: added more route validators

--- src/middleware/validators.js
+++ src/middleware/validators.js
@@ -6,6 +6,11 @@ module.exports = {
     /* Auth route validators */
     signUp: validate(authValidations.signUp),
     signin: validate(authValidations.signin),
+    resendOTP: validate(authValidations.resendOTP),
+    verifyAccount: validate(authValidations.verifyAccount),
+    sendPasswordRecoveryEmail: validate(authValidations.sendPasswordRecoveryMail),
+    changePassword: validate(authValidations.changePassword),
+    profileUpdate: validate(authValidations.profileUpdate),
 
     /* Applications route validators */
     apply: validate(applicationValidations.apply)

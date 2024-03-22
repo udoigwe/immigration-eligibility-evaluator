01668525a5553fb9e925fb0b2309325a0aad6d0b
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-03-22 06:29:22 +0100

    sprint 1: added more authentication routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,5 +6,10 @@ const checkAuth = require('../middleware/checkAuth');
 
 router.post('/sign-up', validators.signUp, authController.signUp);
 router.post('/login', validators.signin, authController.signin);
+router.post('/resend-otp', validators.resendOTP, authController.resendOTP);
+router.post('/send-password-recovery-mail', validators.sendPasswordRecoveryEmail, authController.sendPasswordRecoveryMail);
+router.post('/verify-account', validators.verifyAccount, authController.verifyAccount);
+router.post('/change-password', validators.changePassword, authController.changePassword);
+router.post('/profile-update', checkAuth.verifyToken, validators.profileUpdate, authController.profileUpdate);
 
 module.exports = router;

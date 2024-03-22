b0f86d64b0a872c6e97d0b15e89d6ebaf17cd747
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-03-22 06:26:45 +0100

    sprint 1: added more validation logic to auth.validation

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -13,20 +13,9 @@ module.exports = {
             .withMessage("User Email is required")
             .isEmail()
             .withMessage("Please provide a valid email address"),
-        body("gender")
-            .exists({ checkFalsy: true })
-            .withMessage("User Gender is required"),
         body("password")
             .exists({ checkFalsy: true })
-            .withMessage("Password is required"),
-        body("country_id")
-            .exists({ checkFalsy: true })
-            .withMessage("Nationality is required")
-            .isNumeric()
-            .withMessage("Nationality must be digits"),
-        body("dob")
-            .exists({ checkFalsy: true })
-            .withMessage("Date Of Birth is required")
+            .withMessage("Password is required")
 	],
     signin: [
         body("email")
@@ -37,5 +26,63 @@ module.exports = {
         body("password")
             .exists({ checkFalsy: true })
             .withMessage("Password is required")
-    ]
+    ],
+    resendOTP: [
+		body("email")
+			.exists({ checkFalsy: true })
+			.withMessage("Email is required")
+	],
+    verifyAccount: [
+		body("email")
+			.exists({ checkFalsy: true })
+			.withMessage("Email is required"),
+		body("otp")
+			.exists({ checkFalsy: true })
+			.withMessage("OTP is required")
+            .isNumeric()
+            .withMessage("OTP must be a number")
+	],
+    sendPasswordRecoveryMail: [
+		body("email")
+			.exists({ checkFalsy: true })
+			.withMessage("Email is required")
+            .isEmail()
+            .withMessage("Please provide a valid email address")
+	],
+    changePassword: [
+		body("email")
+			.exists({ checkFalsy: true })
+			.withMessage("Email is required")
+            .isEmail()
+            .withMessage("Please provide a valid email address"),
+		body("salt")
+			.exists({ checkFalsy: true })
+			.withMessage("Salt is required"),
+		body("password")
+			.exists({ checkFalsy: true })
+			.withMessage("Password is required"),
+	],
+    profileUpdate: [
+		body("gender")
+			.exists({ checkFalsy: true })
+			.withMessage("Gender is required"),
+		body("date_of_birth")
+			.exists({ checkFalsy: true })
+			.withMessage("Date of birth is required"),
+		body("nationality")
+			.exists({ checkFalsy: true })
+			.withMessage("Nationality is required"),
+		body("marital_status")
+			.exists({ checkFalsy: true })
+			.withMessage("Marital Status is required"),
+		body("education_level")
+			.exists({ checkFalsy: true })
+			.withMessage("Education Level is required"),
+		body("years_of_experience")
+			.exists({ checkFalsy: true })
+			.withMessage("Years Of Experience is required"),
+		body("language_proficiency")
+			.exists({ checkFalsy: true })
+			.withMessage("Language Proficiency is required"),
+	],
 }

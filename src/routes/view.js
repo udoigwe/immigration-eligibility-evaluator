5926aca187194c4f5ffeccb1e4264c2eafe544b8
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-03-22 06:28:53 +0100

    sprint 1: added more view routes

--- src/routes/view.js
+++ src/routes/view.js
@@ -5,6 +5,9 @@ const viewsController = require('../controllers/view.controller');
 router.get('/', viewsController.home);
 router.get('/sign-in', viewsController.signIn);
 router.get('/register', viewsController.signUp);
+router.get('/account-verification', viewsController.verifyAccount);
+router.get('/forgot-password', viewsController.forgotPassword);
+router.get('/password-recovery', viewsController.passwordRecovery);
 
 /* Protected pages */
 router.get('/dashboard', viewsController.dashboard);

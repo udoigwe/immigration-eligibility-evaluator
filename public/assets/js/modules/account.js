602d025d7b8b6f1dc659a7690915a0acdab7d2ef
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-03-22 19:20:49 +0100

    Sprint 1: created an account component

---
+++ public/assets/js/modules/account.js
@@ -0,0 +1,64 @@
+$(function () {
+
+    'use strict';
+
+    let token = sessionStorage.getItem('token');
+
+	$(document).ready(function($) {
+        //reset password
+        resetPassword();
+    });
+
+    function resetPassword()
+    {
+        $('#reset-form').on('submit', function(e){
+            e.preventDefault();
+            var form = $(this);
+            var newPass = $("#new_password").val();
+            var confirmPass = $("#re_password").val();
+            var fields = form.find('input.required, select.required');
+            
+            blockUI();
+
+            for(var i=0;i<fields.length;i++)
+            {
+                if(fields[i].value === "")
+                {
+                    /*alert(fields[i].id)*/
+                    unblockUI();
+                    //showSimpleMessage("Attention", `${fields[i].name} is required`, "error");
+                    alert(`${fields[i].name} is required`)
+                    $('#'+fields[i].id).focus();
+                    return false;
+                }
+            }
+            
+            if(confirmPass !== newPass)
+            {
+               alert("Passwords do not match")
+               unblockUI();
+               return false;
+            }
+    
+            $.ajax({
+                type: 'POST',
+                url: API_URL_ROOT+'/reset-password',
+                data: JSON.stringify(form.serializeObject()),
+                dataType: 'json',
+                contentType: 'application/json',
+                headers:{'x-access-token':token},
+                success: function(response)
+                {
+                    form.get(0).reset();
+                    alert(response.message);
+                    unblockUI();
+                },
+                error: function(req, status, err)
+                {
+                    alert(req.responseJSON.message);
+                    unblockUI();
+                }
+            });
+        });
+    }
+}); 

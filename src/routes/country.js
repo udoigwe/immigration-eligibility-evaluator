aa478d8c42face797ff8a576d97cd76548723eb8
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-03-29 15:15:22 +0100

    Sprint 2: removed country.js reoute files and moved all routes to resources

--- src/routes/country.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const countryController = require('../controllers/country.controller');
-const validators = require('../middleware/validators');
-const checkAuth = require('../middleware/checkAuth');
-
-router.get('/countries', countryController.getCountries);
-
-module.exports = router;

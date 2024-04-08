f28978af23b8343f644ecd5308a3f983a5442871
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-04-08 18:56:11 +0100

    sprint 3: created post routes

---
+++ src/routes/post.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../controllers/post.controller');
+const validators = require('../middleware/validators');
+const checkAuth = require('../middleware/checkAuth');
+
+router.post('/posts', checkAuth.verifyToken, validators.createPost, postController.createPost);
+router.get('/posts', checkAuth.verifyToken, postController.getPosts);
+router.post('/reply-post', checkAuth.verifyToken, validators.replyPost, postController.replyPost);
+
+module.exports = router;

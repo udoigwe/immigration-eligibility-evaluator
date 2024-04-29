0aedce4ed4e28e4fa9e2389f1d0a0ef2bd2db292
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-04-29 21:51:55 +0100

    chore: created blog-posts and single blog post routes

--- src/routes/post.js
+++ src/routes/post.js
@@ -8,5 +8,7 @@ router.post('/posts', checkAuth.verifyToken, validators.createPost, postControll
 router.get('/posts', checkAuth.verifyToken, postController.getPosts);
 router.get('/post-categories', checkAuth.verifyToken, postController.getPostCategories);
 router.post('/reply-post', checkAuth.verifyToken, validators.replyPost, postController.replyPost);
+router.get('/blog-posts', checkAuth.verifyToken, postController.getBlogPosts);
+router.get('/blog-posts/:blog_post_id', checkAuth.verifyToken, validators.getBlogPost, postController.getBlogPost);
 
 module.exports = router;

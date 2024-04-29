58289e52e0327450cf61f70646ad41efc2de30bc
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-04-29 21:51:32 +0100

    chore: compare route

--- src/routes/resources.js
+++ src/routes/resources.js
@@ -8,5 +8,6 @@ router.get('/countries', resourcesController.getCountries);
 router.get('/visa-categories', resourcesController.getVisaCategories);
 router.get('/visa-criteria/:country_code/:visa_category_id', validators.getCountryVisaCriteria, resourcesController.getCountryVisaCriteria);
 router.get('/insights/:country_code/:visa_category_id', validators.getImmigrationInsights, resourcesController.getImmigrationInsights);
+router.post('/compare', checkAuth.verifyToken, validators.compareCountries, resourcesController.getCountryVisaComparisom);
 
 module.exports = router;

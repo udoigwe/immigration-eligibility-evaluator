502ff593087b53c693cdb31107e83d15a408997e
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-04-29 21:53:26 +0100

    chore: get blog post controller

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -314,4 +314,116 @@ module.exports = {
             connection ? connection.release() : null;
         }
     },
+    getBlogPosts: async (req, res, next) => {
+
+        const page = req.query.page ? parseInt(req.query.page) : null;
+        const perPage = req.query.perPage ? parseInt(req.query.perPage) : null;
+
+        let connection;
+
+        let query = `
+            SELECT * 
+            FROM blog
+            WHERE 1 = 1
+        `;
+        const queryParams = [];
+
+        let query2 = `
+            SELECT COUNT(*) AS total_records 
+            FROM blog
+            WHERE 1 = 1
+        `;
+        const queryParams2 = []
+
+        query += ` ORDER BY blog_post_id ASC`;
+
+        if(page && perPage)
+        {
+            const offset = (page - 1) * perPage;
+            query += ` LIMIT ?, ?`;
+            queryParams.push(offset);
+            queryParams.push(perPage);
+        }
+
+        try
+        {
+            // Get a connection from the pool
+            connection = await pool.getConnection();
+
+            const [ data ] = await connection.execute(query, queryParams);
+            const [ total ] = await connection.execute(query2, queryParams2);
+
+            //total records
+            const totalRecords = parseInt(total[0].total_records);
+
+            // Calculate total pages if perPage is specified
+            const totalPages = perPage ? Math.ceil(totalRecords / perPage) : null;
+
+            // Calculate next and previous pages based on provided page and totalPages
+            const nextPage = page && totalPages && page < totalPages ? page + 1 : null;
+            const prevPage = page && page > 1 ? page - 1 : null;
+
+            res.json({
+                error: false,
+                data,
+                paginationData: {
+                    totalRecords,
+                    totalPages,
+                    currentPage: page,
+                    itemsPerPage: perPage,
+                    nextPage,
+                    prevPage
+                }
+            })
+        }
+        catch(e)
+        {
+            next(e)
+        }
+        finally
+        {
+            connection ? connection.release() : null;
+        }
+    },
+    getBlogPost: async (req, res, next) => {
+        const loggedUser = req.userDecodedData;
+        const {
+            blog_post_id
+        } = req.params;
+
+        let connection;
+
+        try
+        {
+            // Get a connection from the pool
+            connection = await pool.getConnection();
+
+            //check if post ID exists
+            const [ posts ] = await connection.execute(`
+                SELECT *
+                FROM blog
+                WHERE blog_post_id = ?
+                LIMIT 1`,
+                [ blog_post_id ]
+            )
+
+            if(posts.length === 0)
+            {
+                throw new CustomError(404, "Post not found");
+            }
+
+            res.json({
+                error: false,
+                post: posts[0],
+            })
+        }
+        catch(e)
+        {
+            next(e);
+        }
+        finally
+        {
+            connection ? connection.release() : null;
+        }
+    },
 }
